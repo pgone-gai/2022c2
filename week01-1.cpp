45723c9a4901b0e76dd8144287437d038be9591c
pgone-gai <79628284+pgone-gai@users.noreply.github.com>
2022-02-25 12:06:55 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    printf("Åª¨ì¾ã¼Æ:%d\n",a);
+
+    reaturn 0;
+}
+
---
+++ week01-3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+char[1000]
+int main()
+{
+    scanf("%s",line);
+    for(int i=0;i<100;i++){
+        if(line [i]=='2') printf("§ä¨ì2\n");
+
+    }
+}
+
